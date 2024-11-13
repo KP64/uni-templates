120f3f433b553470d0b90a4515cdb33336e83451
KP64 <karamalsadeh@hotmail.com>
2024-11-13 23:51:20 +0100

    add: alpro treefmt-nix

--- alpro/flake.nix
+++ alpro/flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -11,14 +15,18 @@
       self,
       nixpkgs,
       flake-utils,
+      treefmt-nix,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
         filesToCompile = [
           "main"
         ];
+
+        treefmt = (treefmt-nix.lib.evalModule pkgs ./treefmt.nix).config.build;
       in
       {
         packages.default = pkgs.stdenv.mkDerivation {
@@ -33,11 +41,12 @@
             ''
               mkdir -p $out/bin
             ''
-            + (pkgs.lib.concatLines (map (f: "cp ${f} $out/bin/") filesToCompile));
+            + (pkgs.lib.concatLines (map (f: "cp ${f} $out/bin") filesToCompile));
           nativeBuildInputs = with pkgs; [ clang ];
         };
 
-        formatter = pkgs.nixfmt-rfc-style;
+        formatter = treefmt.wrapper;
+        checks.formatting = treefmt.check self;
 
         devShells.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
           packages =
---
+++ alpro/treefmt.nix
@@ -0,0 +1,13 @@
+{
+  projectRootFile = ".git/config";
+
+  programs = {
+    deadnix.enable = true;
+    statix.enable = true;
+    nixfmt.enable = true;
+
+    clang-format.enable = true;
+
+    just.enable = true;
+  };
+}
