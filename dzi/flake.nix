72772b2ee41c0ca50bb49317f172ae1f2baec951
KP64 <karamalsadeh@hotmail.com>
2025-01-29 08:15:12 +0100

    feat(dzi): Add treefmt-nix

--- dzi/flake.nix
+++ dzi/flake.nix
@@ -4,19 +4,32 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
+      self,
       nixpkgs,
       flake-utils,
+      treefmt-nix,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        treefmt = (treefmt-nix.lib.evalModule pkgs ./treefmt.nix).config.build;
       in
       {
+        formatter = treefmt.wrapper;
+
+        checks.formatting = treefmt.check self;
+
         devShells.default = pkgs.mkShell {
           venvDir = ".venv";
           packages = with pkgs; [
