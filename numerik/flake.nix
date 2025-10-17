e199aa8dd781fbde7256190cb8490272ae1adae2
KP64 <karamalsadeh@hotmail.com>
2025-10-17 17:11:26 +0200

    feat: numerik module

---
+++ numerik/flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Numerik - Winter Semester";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      imports = [ inputs.treefmt-nix.flakeModule ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt.programs = {
+            deadnix.enable = true;
+            statix.enable = true;
+            nixfmt = {
+              enable = true;
+              strict = true;
+            };
+
+            just.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nil
+
+              just
+              just-lsp
+
+              (python313.withPackages (
+                pypkgs: with pypkgs; [
+                  jupyter
+                  matplotlib
+                  numpy
+                  scipy
+                ]
+              ))
+            ];
+          };
+        };
+    };
+}
