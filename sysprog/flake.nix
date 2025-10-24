1706905bdb38805ec6eb3f0fab1ca639e8225056
KP64 <karamalsadeh@hotmail.com>
2025-10-25 01:28:50 +0200

    feat: sysprog module

---
+++ sysprog/flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "SysProg - Winter Semester";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      imports = [ inputs.treefmt-nix.flakeModule ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt.programs = {
+            deadnix.enable = true;
+            statix.enable = true;
+            nixfmt = {
+              enable = true;
+              strict = true;
+            };
+
+            asmfmt.enable = true;
+
+            clang-format.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            packages = with pkgs; [
+              nil
+
+              clang-tools
+
+              lldb
+              valgrind
+              nasm
+            ];
+          };
+        };
+    };
+}
