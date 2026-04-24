0ac7dcc58b474f9e1fd9130f90715c57022ad7fa
KP64 <karamalsadeh@hotmail.com>
2026-04-24 20:01:19 +0200

    feat: add netsi module

---
+++ netsi/flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "NetSi - Sommer Semester";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+      imports = [ inputs.treefmt-nix.flakeModule ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt.programs = {
+            deadnix.enable = true;
+            statix.enable = true;
+            nixf-diagnose.enable = true;
+            nixfmt = {
+              enable = true;
+              strict = true;
+            };
+
+            shellcheck.enable = true;
+            shfmt.enable = true;
+
+            texfmt.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            name = "NetSi";
+
+            packages =
+              let
+                # NOTE: Add tex packages here if you have to
+                tex = pkgs.texliveFull.withPackages (ps: [ ]);
+              in
+              [
+                pkgs.nil
+                tex
+              ];
+          };
+        };
+    };
+}
